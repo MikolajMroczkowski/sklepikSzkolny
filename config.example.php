de1ecafa9d565a93868de5d4c046fd9a50d80ed6
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2022-01-24 12:19:10 +0100

    fixed config

--- config.example.php
+++ config.example.php
@@ -1,1 +1,6 @@
 <?php
+$dbAdress = "127.0.0.1";
+$dbName = "sklepik";
+$dbUser = "shop";
+$dbPass = "";
+$workDir = "/home/miki/PhpstormProjects/sklepikSzkolny";
--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$appName = "Test";
-$dbAdress = "127.0.0.1:3307";
-$dbName = "sklepik";
-$dbUser = "shop";
-$dbPass = "u_3[P!7/CtPQ9_(8";
-$workDir = "/home/miki/PhpstormProjects/sklepikSzkolny";
